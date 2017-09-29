adc10b3a432e6f7fdd4afc559b12e246e78a6fda
jeromed <jeromed@askia.com>
2017-09-29 17:16:29 +0200

    v2.0.1

--- resources/dynamic/Initializer.js
+++ resources/dynamic/Initializer.js
@@ -34,20 +34,12 @@
         adjustXtextObj : [{%:= adjustX %}],
         adjustYtextObj : [{%:= adjustY %}],
         currentQuestion: '{%:= CurrentQuestion.Shortcut %}',
-        mapCaptions : [
-        {%
-		Dim i
+        mapCaptions : [{% Dim i
         For i = 1 To CurrentQuestion.Responses.Count
-		%}'{%:= CurrentQuestion.Responses[i].Caption %}'{%:= On((i <> CurrentQuestion.Responses.Count),",","") %}{% Next i %}
-        ],
-        mapNames : [
-        {%
-		Dim j
+		%}'{%:= CurrentQuestion.Responses[i].Caption %}'{%:= On((i <> CurrentQuestion.Responses.Count),",","") %}{% Next i %}],
+        mapNames : [{% Dim j
         For j = 1 To CurrentQuestion.Responses.Count
-		%}'zone{%:= CurrentQuestion.Responses[j].InputValue() %}'{%:= On((i <> CurrentQuestion.Responses.Count),",","") %}{% Next j %}
-        ],
-        mapPaths : [
-        {%:= CurrentADC.GetContent("dynamic/" + pathFileName + ".txt").ToText() %}
-        ]
+		%}'zone{%:= CurrentQuestion.Responses[j].InputValue() %}'{%:= On((i <> CurrentQuestion.Responses.Count),",","") %}{% Next j %}],
+        mapPaths : [{%:= CurrentADC.GetContent("dynamic/" + pathFileName + ".txt").ToText() %}]
     });
 } ());
